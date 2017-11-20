5c4cc82dadd5146f46b02cbfdeeb75acc053c26c
Christopher Silich <chrissilich@gmail.com>
2017-11-20 10:25:06 -0500

    todo list blank assignment

---
+++ assets/js/main.js
@@ -0,0 +1,14 @@
+"use strict";
+
+console.log("Hello World from main.js!");
+
+// your homework is to make this todo list a little more useful by showing which items are higher priority than others.
+// look in the HTML, they have classes that designate their priority, 
+
+// 1. make the "total" span in the stats div say the correct number
+
+// 2. make the "high" span in the stats div say the correct number
+
+// 3. for each of the items that are marked as high priority
+// 3a. make the text red and a little larger.
+// 3b. change the text to have and exclamation mark on the end
